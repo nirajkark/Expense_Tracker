593d4764772f7cb129035ac9907cc5cdf51fe46b
nirajkark <nirajkarki416124@gmail.com>
2024-06-17 10:53:15 +0545

    login page updated

---
+++ Frontend/script.js
@@ -0,0 +1,7 @@
+document.getElementById('login').addEventListener('click', function() {
+    document.getElementById('login-container').style.display = 'flex';
+});
+document.getElementById('Home').addEventListener('click', function() {
+    document.getElementById('login-container').style.display = 'flex';
+});
+
